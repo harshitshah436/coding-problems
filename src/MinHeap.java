4294c702624f7d9af2a909c210cc8e48d41142ff
harshitshah436 <harshitshah436@yahoo.com>
2017-03-26 16:44:54 -0400

    Minimum Heap implementation

---
+++ src/MinHeap.java
@@ -0,0 +1,115 @@
+/**
+ * MinHeap problem has two main operation while implementation.
+ * 1) insert - (insert at last position, right most leaf, then bubble up till root)
+ * 2) extract Min (remove from root, then bubble down/minHeapify)
+ *
+ * @author Harshit
+ */
+public class MinHeap {
+
+    private int[] Heap;
+    private int size;
+    private int maxsize;
+
+    private static final int FRONT = 1;
+
+    public MinHeap(int maxsize) {
+        this.maxsize = maxsize;
+        this.size = 0;
+        Heap = new int[this.maxsize + 1];
+        Heap[0] = Integer.MIN_VALUE;
+    }
+
+    public void insert(int element) {
+        Heap[++size] = element;
+        bubbleUp();
+    }
+
+    public int extractMin() {
+        int popped = Heap[FRONT];
+        Heap[FRONT] = Heap[size--];
+        minHeapify(FRONT);
+        return popped;
+    }
+
+    public void bubbleUp() {
+        int current = size;
+
+        while (Heap[current] < Heap[parent(current)]) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+
+    /**
+     * BubbleDown method
+     */
+    private void minHeapify(int pos) {
+        if (!isLeaf(pos)) {
+            if (Heap[pos] > Heap[leftChild(pos)] || Heap[pos] > Heap[rightChild(pos)]) {
+                if (Heap[leftChild(pos)] < Heap[rightChild(pos)]) {
+                    swap(pos, leftChild(pos));
+                    minHeapify(leftChild(pos));
+                } else {
+                    swap(pos, rightChild(pos));
+                    minHeapify(rightChild(pos));
+                }
+            }
+        }
+    }
+
+    public void minHeap() {
+        for (int pos = (size / 2); pos >= 1; pos--) {
+            minHeapify(pos);
+        }
+    }
+
+    private int parent(int pos) {
+        return pos / 2;
+    }
+
+    private int leftChild(int pos) {
+        return (2 * pos);
+    }
+
+    private int rightChild(int pos) {
+        return (2 * pos) + 1;
+    }
+
+    private boolean isLeaf(int pos) {
+        return pos >= (size / 2) && pos <= size;
+    }
+
+    private void swap(int fpos, int spos) {
+        int tmp;
+        tmp = Heap[fpos];
+        Heap[fpos] = Heap[spos];
+        Heap[spos] = tmp;
+    }
+
+    public void print() {
+        for (int i = 1; i <= size / 2; i++) {
+            System.out.print(" PARENT : " + Heap[i] + " LEFT CHILD : " + Heap[2 * i]
+                    + " RIGHT CHILD :" + Heap[2 * i + 1]);
+            System.out.println();
+        }
+    }
+
+    public static void main(String... arg) {
+        System.out.println("The Min Heap is ");
+        MinHeap minHeap = new MinHeap(15);
+        minHeap.insert(5);
+        minHeap.insert(3);
+        minHeap.insert(17);
+        minHeap.insert(10);
+        minHeap.insert(84);
+        minHeap.insert(19);
+        minHeap.insert(6);
+        minHeap.insert(22);
+        minHeap.insert(9);
+        minHeap.minHeap();
+
+        minHeap.print();
+        System.out.println("The Min val is " + minHeap.extractMin());
+    }
+}
