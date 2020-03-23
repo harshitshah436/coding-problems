048238deda30f8263f07dccd7a14d365f673e188
Harshit Shah <harshitshah436@yahoo.com>
2020-03-22 20:26:32 -0400

    Update StringBuilderUsage.java

--- src/basics/java/StringBuilderUsage.java
+++ src/basics/java/StringBuilderUsage.java
@@ -16,18 +16,38 @@ public class StringBuilderUsage {
     public static String getUniqueStringUsingStringBuilder(String s) {
         StringBuilder sb = new StringBuilder();
         for (char c : s.toCharArray()) {
+            // Use of indexOf method
             if (sb.indexOf(String.valueOf(c)) == -1) {
                 sb.append(c);
             }
         }
         return sb.toString();
     }
-    
+
     public static void main(String[] args) {
-        // Suppose we want to make a string of Unique chars we don't know the 
+        // Suppose we want to make a string of Unique chars we don't know the
         // size of resultant string.
         String s = "harshitshah";
         s = getUniqueStringUsingStringBuilder(s);
         System.out.println(s);
+
+        StringBuilder sb = new StringBuilder();
+        // Default StringBuilder capacity - 16
+        System.out.println(sb.capacity());
+        sb.append("Aarshit");
+        System.out.println(sb.capacity());
+
+        // Reverse the string using StringBuilder
+        System.out.println(sb.reverse());
+        System.out.println(sb);
+
+        // Unicode char value (ASCII)
+        System.out.println(sb.codePointAt(0) + "  " + sb.codePointAt(1));
+        // All char Unicode values
+        sb.chars().mapToObj(p -> p + " ").forEach(System.out::print);
+//        sb.chars().forEach(System.out::println);        
+
+        // Other methods are same as String class: length(), substring(i),
+        // substring(i,i), charAt(i)
     }
 }
