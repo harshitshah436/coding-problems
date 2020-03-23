de14be9b6f03388267993c984ad461515892f8d5
Harshit Shah <harshitshah436@yahoo.com>
2020-03-22 20:26:05 -0400

    Update StringAnagrams.java
    Use only 1 frequency array

--- src/strings/StringAnagrams.java
+++ src/strings/StringAnagrams.java
@@ -18,6 +18,9 @@ public class StringAnagrams {
 
         if (string1.length() != string2.length())
             return false;
+        
+        string1 = string1.toLowerCase();
+        string2 = string2.toLowerCase();
 
         // Create 2 count arrays and initialize all values as 0
         int[] count1 = new int[26];
@@ -43,8 +46,38 @@ public class StringAnagrams {
         return true;
     }
 
+    public static boolean areAnagram1(String str1, String str2) {
+
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        // Create a frequency/count array and initialize all values with 0
+        char[] freq = new char[26];
+        
+        // For input strings, increase count for one string and decrease for
+        // another.
+        for (int i = 0; i < str1.length(); i++) {
+            freq[str1.codePointAt(i) - 97]++;
+            freq[str2.codePointAt(i) - 97]--;
+        }
+
+        // Check any non-zero value
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
         System.out.println(areAnagram("harshitshah", "shahharshit"));
+        System.out.println(areAnagram1("harshitshah", "shahharshit"));
     }
 
 }
